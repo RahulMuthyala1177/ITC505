3bcadcb57082051ae96a715fb918cf817c94f0ca
RahulMuthyala1177 <157927441+RahulMuthyala1177@users.noreply.github.com>
2024-04-22 21:58:20 -0700

    Create script.js

---
+++ Lab-8/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("registrationForm").onsubmit = function(event) {
+        var password = document.getElementById("password").value;
+        var confirmPassword = document.getElementById("confirmPassword").value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match.");
+            event.preventDefault();
+        }
+    };
+});
